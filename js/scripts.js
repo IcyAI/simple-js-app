2f6f403c841a567ec857813abb9840907e659996
IcyAI <alvinob1515@gmail.com>
2023-11-10 16:07:02 -0500

    Added new array including 3 pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,3 @@
 
+let pokemonList= [{name: 'Charmander', height: 2, types: ['fire']}, {name: 'Squirtle', height: 3, types: ['water']}, {name: 'Bulbasaur', height: 1, types: ['grass', 'poison'] }];
+ 
