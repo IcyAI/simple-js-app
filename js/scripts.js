b178391ef3ba4604b4ff3d43f83be8047c6a6ec8
IcyAI <alvinob1515@gmail.com>
2023-11-14 20:47:12 -0500

    changed format of  the Pokémon array objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
 
-let pokemonList= [{name: 'Charmander', height: 2, types: ['fire']}, {name: 'Squirtle', height: 3, types: ['water']}, {name: 'Bulbasaur', height: 1, types: ['grass', 'poison'] }];
- 
 //array list of all my pokemon and attributes
 let pokemonList = [
     {name: 'Charmander', height: 2, types: ['fire']}, 
