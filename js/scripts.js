c55599594a29a075a12f6fa21041f2f935e0e754
IcyAI <alvinob1515@gmail.com>
2023-11-14 20:44:28 -0500

    Separated my objects in the Pokémon array to different lines

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 
 let pokemonList= [{name: 'Charmander', height: 2, types: ['fire']}, {name: 'Squirtle', height: 3, types: ['water']}, {name: 'Bulbasaur', height: 1, types: ['grass', 'poison'] }];
  
+//array list of all my pokemon and attributes
+let pokemonList = [
+    {name: 'Charmander', height: 2, types: ['fire']}, 
+    {name: 'Squirtle', height: 3, types: ['water']}, 
+    {name: 'Bulbasaur', height: 1, types: ['grass', 'poison'] }
+];
+
