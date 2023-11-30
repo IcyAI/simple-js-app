87d40cb384440ab19a8a5c15fa6eee1c98b32c30
IcyAI <alvinob1515@gmail.com>
2023-11-29 19:31:50 -0500

    Saved pokemon to variable to be printed and added line breaks

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,27 @@
 
 //array list of all my pokemon and attributes
 let pokemonList = [
-    {name: 'Charmander', height: 2, types: ['fire']}, 
-    {name: 'Squirtle', height: 3, types: ['water']}, 
-    {name: 'Bulbasaur', height: 1, types: ['grass', 'poison'] }
+    { name: 'Charmander', height: 2, types: ['fire'] },
+    { name: 'Squirtle', height: 3, types: ['water'] },
+    { name: 'Bulbasaur', height: 1, types: ['grass', 'poison'] }
 ];
 
 //for loop to print out all pokemon in list
 for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + ',' + ' ' + '(height' + ' ' + pokemonList[i].height + ')');
 
-    //if statment to check if pokemon are taller then 2 units
-    if (pokemonList[i].height > 2) {
-        document.write(' - Wow, that\'s big!');
+    let Pokemon = pokemonList[i].name + ',' + ' ' + 'height' + ' ' + pokemonList[i].height;
+
+    if (pokemonList[i].height <= 2) { 
+
+    document.write(Pokemon + '<br/>');
+
     }
+
+//if statment to check if pokemon are taller then 2 units
+else if (pokemonList[i].height > 2) {
+
+    document.write(Pokemon + ' - Wow, that\'s big!' + '<br/>');
+
+}
+    
 }
