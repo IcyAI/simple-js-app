d63065bb8f424b102f4ba9008070c75ab81f0c21
IcyAI <alvinob1515@gmail.com>
2023-11-29 20:38:09 -0500

    Changed for loop to forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -8,23 +8,14 @@ let pokemonList = [
     { name: 'Bulbasaur', height: 1, types: ['grass', 'poison'] }
 ];
 
-//for loop to print out all pokemon in list
-for (let i = 0; i < pokemonList.length; i++) {
 
-    let Pokemon = pokemonList[i].name + ',' + ' ' + 'height' + ' ' + pokemonList[i].height;
 
-    if (pokemonList[i].height <= 2) { 
 
     document.write(Pokemon + '<br/>');
 })();
 
-    }
 
-//if statment to check if pokemon are taller then 2 units
-else if (pokemonList[i].height > 2) {
 
-    document.write(Pokemon + ' - Wow, that\'s big!' + '<br/>');
 
 }
     
-}
