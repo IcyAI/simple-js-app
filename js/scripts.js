2b396d7dd460c8a3cf5d3eea115f56274365f914
IcyAI <148020399+IcyAI@users.noreply.github.com>
2023-12-16 18:34:06 -0500

    Added close modal when clicking overlay

--- js/scripts.js
+++ js/scripts.js
@@ -99,6 +99,15 @@ let pokemonRepository = (function () {
 
     modalContainer.classList.add('is-visible');
 
+    modalContainer.addEventListener('click', (e) => {
+        //Since this is also triggered when clicking inside the modal, we only want to close if the user clicks directly on the overlay
+        let target= e.target;
+        if(target === modalContainer) {
+            hideModal();
+        }
+    });
+}
+
 function hideModal() {
     let modalContainer= document.querySelector('#modal-container');
     modalContainer.classList.remove('is-visible');
